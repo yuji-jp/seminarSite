d3e65ccd540adcd2f4b1d97e2ca0ad1862ab7892
yuji-jp <106308346+yuji-jp@users.noreply.github.com>
2022-06-24 12:20:29 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,4 +33,36 @@ $('a[href^="#"]').on('click', function () {
     $('.humburger , .nav-list , .humburger-line').toggleClass('open');
   });
 
+  //フォームエラーチェック//
+  $('.js-btn').on('click',function(){
+    $('#type-error').text("");
+    $('#name-error').text("");
+    $('#tel-error').text("");
+    $('#email-error').text("");
+    $('#comment-error').text("");
+    $('#job-error').text("");
+
+    if($('input[name="type"]').val() == "") {
+      $("#type-error").text("選択してください。");
+    }
+
+    if($('input[name="name"]').val() == "") {
+      $('#name-error').text("お名前をご入力してください。");
+    }
+
+    if($('input[name=email]').val()=="") {
+      $('#email-error').text("メールアドレスをご入力してください");
+    }
+
+    if($('input[name=tel]').val() == "") {
+      $('#tel-error').text("お電話番号をご入力してください");
+    }
+
+    if($('textarea[name=comment]').val() == "") {
+      $('#comment-error').text("内容をお書きください")
+    }
+
+    if($('select[name=job]').val() == "") {
+      $('#job-error').text("選択してください")
+    }
 });
